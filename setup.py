4113e81f898251aaa89ed50e191d83c912ee35d4
arfath pasha <arfathpasha@gmail.com>
2019-05-29 14:57:16 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/arfathpasha/spectrum_etl',
+    url='https://github.com/shahcompbio/spectrum_etl',
     version='0.1.0',
     zip_safe=False,
 )
