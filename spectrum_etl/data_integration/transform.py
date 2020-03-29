1379122d85d8258f59e4a1c85dd4307371a1cb19
Lim <limj@lbst0123.mskcc.root.mskcc.org>
2020-03-29 16:24:15 -0400

    updated formatting

--- spectrum_etl/data_integration/transform.py
+++ spectrum_etl/data_integration/transform.py
@@ -21,35 +21,53 @@ class Transformation(object):
         pd.set_option('display.max_columns', None)
         pd.set_option('display.width', None)
         pd.set_option('display.max_colwidth', -1)
-        eldf = Elab_DataFrame('filtered_elab_sample_data')
-        rcdf = RedCap_DataFrame('hne_metadata')
-        final_df = eldf.merge(rcdf)
-        print(final_df)
+        self.eldf = Elab_DataFrame('filtered_elab_sample_data')
+        self.rcdf = RedCap_DataFrame('hne_metadata')
+
+    def transform(self):
+        final_df = self.eldf.merge(self.rcdf)
+
+        with open("output_for_drill", 'w') as outfile:
+            final_df.to_json(path_or_buf=outfile, orient='table')
+
 
 class DataFrame:
     '''
-    This is an experimental class for the import and transformation of genomic data and pathology data various databases
-    into a dataframe.
+    A high level abstraction of a pandas dataframe for multi-modal data.
     '''
 
     def __init__(self, file_name):
-        self.dataframe = self.create_dataframe(file_name)
+        self.dataframe = self.__create_dataframe(file_name)
 
-    def create_dataframe(self, file_name):
+    def __create_dataframe(self, file_name):
         with open(file_name) as data_file:
             metadata = json.load(data_file)
         return pd.DataFrame(metadata)
 
     def site_transform(self, specimen_site, site_details):
+        '''
+        Replaces all NaNs with empty strings, replaces specimen_site with values of site_details if available
+        @:param specimen_site column name of specimen site in this table
+        @:param site_details column name of site details in this table
+        :returns the transformed dataframe
+        '''
+
         df = self.dataframe
         df_blank = df.replace(np.nan, '', regex=True)
         df_blank.loc[df_blank[site_details] != "", specimen_site] = df_blank[site_details]
         self.df_blank = df_blank
+        return self.df_blank
 
     def merge(self, other_df):
+        '''
+        Merges the dataframe based on preferred column headers and sets and sorts the index of merged table to Patient ID and Specimen Site
+        @:param other_df the data frame to be merged into the object
+        :returns the merged dataframe
+        '''
+
         merged_df = pd.merge(self.df_blank, other_df.df_blank, how='left', left_on=['Patient ID', 'Specimen Site'], right_on=['patient_id', 'gyn_pathology_specimen_site'])
         final_df = merged_df.set_index(["Patient ID", "Specimen Site"]).sort_values(['Patient ID', 'Specimen Site'], ascending=True)
-        # upon merge, Specimen Site pulls from the elab version, so the detailed from redcap specimen site does not merge
+        # upon merge, Specimen Site pulls from the elab version, so the details from redcap specimen site does not merge
         # how do we transfer redcaps specimen site to the merged table as well?
         return final_df
 
@@ -59,7 +77,7 @@ class Elab_DataFrame(DataFrame):
     '''
 
     def __init__(self, file_name):
-        DataFrame.__init__(self, file_name)
+        super().__init__(file_name)
         self.site_transform()
 
     def site_transform(self, specimen_site="Specimen Site", site_details="Site Details"):
@@ -71,22 +89,28 @@ class RedCap_DataFrame(DataFrame):
     '''
 
     def __init__(self, file_name):
-        DataFrame.__init__(self, file_name)
-        self.format_redcap_dataframe()
+        super().__init__(file_name)
+        self.__format_redcap_dataframe()
         self.site_transform()
 
-    def format_column_header(self, header):
+    def __format_column_header(self, header):
+        '''
+        Capitalizes the headers of each column
+        '''
         self.dataframe[header] = self.dataframe[header].str.title()
 
-    def format_redcap_dataframe(self):
+    def __format_redcap_dataframe(self):
+        '''
+        Removes rows with post-Rx patient data
+        '''
         indexNames = self.dataframe[self.dataframe['gyn_pathology_therapy'] == "post-Rx"].index
         self.dataframe.drop(indexNames, inplace=True)
 
-        self.format_column_header('gyn_pathology_specimen_site')
-        self.format_column_header('gyn_pathology_specimen_subsite')
+        self.__format_column_header('gyn_pathology_specimen_site')
+        self.__format_column_header('gyn_pathology_specimen_subsite')
 
     def site_transform(self, specimen_site="gyn_pathology_specimen_site", site_details="gyn_pathology_specimen_subsite"):
         self.dataframe = self.dataframe.replace({'gyn_pathology_specimen_site':
                                    {'Omentum': 'Infracolic Omentum',
                                     'Peritoneum': 'Pelvic Peritoneum'}})
-        DataFrame.site_transform(self, specimen_site, site_details)
+        super().site_transform(specimen_site, site_details)
