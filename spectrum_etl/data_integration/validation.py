f4034bd4364e3a79a4bec1efe739c7a5e421fbd6
limj <limj@mskcc.org>
2020-05-06 12:41:27 -0700

    new validation script

--- spectrum_etl/data_integration/validation.py
+++ spectrum_etl/data_integration/validation.py
@@ -124,3 +124,51 @@ def is_scRNA_REX_ID(scrna_rex_id, patient_id, qc_checks):
     if scrna_rex_id != "NULL" and qc_checks == "NULL":
         print("Please ensure input for QC Checks are valid for %s." % patient_id)
         sys.exit(1)
+
+# validate DLP REX ID
+def is_DLP_REX_ID(dlp_rex_id, patient_id):
+    if dlp_rex_id != re.compile(r"\b\d{3}[A-Z]{2,3}_DLP\b"):
+        print("Please ensure DLP REX ID is in proper format for %s." % patient_id)
+        sys.exit(1)
+
+# validate tissue type for WGS bulk tumour
+def is_WGS_tissue_type_valid(tissue_type, patient_id):
+    if tissue_type != "Frozen Tissue":
+        print("Please ensure WGS bulk tumour tissue type is accurate for %s." % patient_id)
+        sys.exit(1)
+
+# validate PPBC accession # for frozen/FFPE
+def is_PPBC_acc_num_valid(ppbc_acc_num, patient_id):
+    if ppbc_acc_num != re.compile(r"\bS\d{2}-\d{5}\b"):
+        print("Please ensure PPBC accession number is in proper format for %s." % patient_id)
+        sys.exit(1)
+
+# validate PPBC bank # for frozen/FFPE
+def is_ppbc_bank_num_valid(ppbc_bank_num, patient_id):
+    if ppbc_bank_num != re.compile(r"\bTS-\d{5}\b"):
+        print("Please ensure PPBC bank number is in proper format for %s." % patient_id)
+        sys.exit(1)
+
+# validate WGS IGO ID
+def is_WGS_IGO_ID_valid(wgs_igo_id, patient_id):
+    if wgs_igo_id != re.compile(r"\b[A-Z]{1,2}\b"):
+        print("Please ensure WGS IGO ID is in proper format for %s." % patient_id)
+        sys.exit(1)
+
+# validate WGS IGO Submission ID
+def is_WGS_IGO_Submission_ID_valid(wgs_igo_sub_id, patient_id):
+    if wgs_igo_sub_id != re.compile(r"\bIGO-\d{6}\b"):
+        print("Please ensure WGS IGO Submission ID is in proper format for %s." % patient_id)
+        sys.exit(1)
+
+# validate WGS REX ID
+def is_WGS_REX_ID(wgs_rex_id, patient_id):
+    if wgs_rex_id != re.compile(r"\b\d{3}[A-Z]{2,3}_T\b"):
+        print("Please ensure WGS REX ID is in proper format for %s." % patient_id)
+        sys.exit(1)
+
+# validate tissue type for IF
+def is_IF_tissue_type_valid(tissue_type, patient_id):
+    if tissue_type != "FFPE Block":
+        print("Please ensure IF tissue type is accurate for %s." % patient_id)
+        sys.exit(1)
